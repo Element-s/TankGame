c42b6d4c29adee4b19575c6bf4c530348f5403e4
elements <qxligleam@gmail.com>
2013-05-17 00:02:39 +0800

    add bullet file,del invalid commit

---
+++ src/bullet.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+'''
+Created on 2013-5-16
+
+@author: Elem
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self,params):
+        '''
+        Constructor
+        '''
+        
